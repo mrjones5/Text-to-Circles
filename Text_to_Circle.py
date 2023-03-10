11989dcf1859b1c555d725caf9cc969c88c7393d
Miranda Jones <90801930+mrjones5@users.noreply.github.com>
2023-03-09 21:31:08 -0600

    First upload

---
+++ Text_to_Circle.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  1 21:49:41 2023
+
+@author: mirandajones
+"""
+
+import matplotlib.pyplot as plt
+import math
+import pandas as pd
+import re
+import numpy as np
+import streamlit as st
+
+#Make a simple app that takes a few inputs and generates an image
+
+st.header("Translate Text to Colored Circles")
+
+st.write("### Wait, what!? Each letter of the alphabet has been mapped to an rgba \
+         color. Enter any text to generate an image!")
+
+input = st.text_input("Enter text", "ABC")
+plt.subplots(sharex=True, sharey=True)
+
+
+input = input.replace(' ', '')
+input=re.sub("[^A-Za-z]","",input)
+lower_input = input.lower()
+name = list(lower_input)
+name_length = len(name)
+
+print(name)
+
+#generate rgba codes
+
+min = 48
+
+max = 235
+
+rgba_r = np.empty(26)
+rgba_g = np.empty(26)
+rgba_b = np.empty(26)
+rgba_a = np.ones(26)
+
+#Now fill the arrays
+for row, element in enumerate(rgba_r):
+    if row <= 5:
+        rgba_r[row]=max
+    if row > 5 and row <= 9:
+        rgba_r[row] = int(max - (row-5)*(max - min)/5)
+    if row > 9 and row <= 17:
+        rgba_r[row]= min
+    if row > 17 and row <= 22:
+        rgba_r[row] = int(min + (row-17)*(max - min)/5)
+    if row > 22:
+        rgba_r[row]=max
+
+for row, element in enumerate(rgba_g):
+    if row <= 4:
+        rgba_g[row]= int(min + row*(max - min)/5)
+    if row > 4 and row <= 13:
+        rgba_g[row] = max
+    if row > 13 and row <= 17:
+        rgba_g[row]= int(max - (row-13)*(max - min)/5)
+    if row > 17:
+        rgba_g[row] = min
+
+for row, element in enumerate(rgba_b):
+    if row <= 8:
+        rgba_b[row]=min
+    if row > 8 and row <= 13:
+        rgba_b[row] = int(min + (row-8)*(max - min)/5)
+    if row > 13 and row <= 21:
+        rgba_b[row]= max
+    if row > 21:
+        rgba_b[row] = int(max - (row-21)*(max - min)/5)
+
+letter_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] 
+letter = np.asarray(letter_list)
+
+rgba = np.c_[rgba_r,rgba_g,rgba_b,rgba_a,letter]
+
+
+rgba_df = pd.DataFrame(rgba)
+
+rgba_df.columns =['R', 'G', 'B', 'A','letter']
+
+rgba_df["R"] =  rgba_df["R"].astype(float)
+rgba_df["R"] = rgba_df["R"].astype(int)
+
+rgba_df["G"] =  rgba_df["G"].astype(float)
+rgba_df["G"] = rgba_df["G"].astype(int)
+
+rgba_df["B"] =  rgba_df["B"].astype(float)
+rgba_df["B"] = rgba_df["B"].astype(int)
+
+rgba_df["A"] =  rgba_df["A"].astype(float)
+rgba_df["A"] = rgba_df["A"].astype(int)
+
+# loop through the length of tickers and keep track of index
+for n, letter in enumerate(name):
+    # add a new subplot iteratively
+    
+    ax = plt.subplot(round(math.sqrt(name_length),0), round(math.sqrt(name_length),0)+1, n + 1)
+
+    # filter df and plot ticker on the new subplot axis
+    mapped_value = ord(letter)-96
+    rgba_row = rgba_df[rgba_df['letter']==letter]
+    rgba_row = rgba_row.reset_index()
+    hue = [int(rgba_row['R'][0])/249,int(rgba_row['G'][0])/249,int(rgba_row['B'][0])/249,1]
+    #rectangle = plt.Rectangle((0,0), 20, 20, fc=hue)
+    circle = plt.Circle(( 0.5 , 0.5 ), .5 , fc=hue)
+    plt.gca().add_patch(circle)
+    plt.gca().set_xticklabels([])
+    plt.gca().set_yticklabels([])
+    #plt.gca().set_aspect('equal')
+    #plt.axis('scaled')
+    plt.axis('off')
+
+plt.subplots_adjust(wspace=0.05, hspace=0.05)    
