08c1e0be8412375293bfa8f749c1da8e5dc61123
Miranda Jones <90801930+mrjones5@users.noreply.github.com>
2023-03-21 20:58:52 -0500

    Update Text_to_Circle.py

--- Text_to_Circle.py
+++ Text_to_Circle.py
@@ -149,5 +149,5 @@ if st.button('Encrpyt Message'):
 
 #Disclaimer
 st.write("Special Note: I do not recommend using this for real message encryption.\
-It is just meant to be a fun demonstration. If you want the backstory, you can \
-read more in my GitHub Wiki.")
+ It is just meant to be a fun demonstration. If you want the backstory, you can \
+read more in my GitHub Wiki. https://github.com/mrjones5/Text-to-Circles")
